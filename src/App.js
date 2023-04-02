14f39bd094be29bf99ba69462b132303f4ce63c2
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-04-03 01:28:58 +0530

    done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Jumbutron from "./components/Jumbutron";
+import SearchField from "./components/SearchField";
+import Images from "./components/Images";
+import useAxios from "./hooks/useAxios";
+import { createContext, useState } from "react";
+
+// Create Context
+
+export const ImageContext = createContext();
 
 function App() {
+  const [searchImage, setSearchImage] = useState("");
+  const { response, isLoading, error, fetchData } = useAxios(
+    `search/photos?page=2&query=india&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+  );
+
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData,
+    searchImage,
+    setSearchImage,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ImageContext.Provider value={value}>
+      <Jumbutron>
+        <SearchField />
+      </Jumbutron>
+      <Images />
+    </ImageContext.Provider>
   );
 }
 
---
+++ src/components/SearchField.js
@@ -0,0 +1,51 @@
+import React, { useContext, useState } from "react";
+import { ImageContext } from "../App";
+
+const SearchField = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const { fetchData, setSearchImage } = useContext(ImageContext);
+
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleButtonSearch = () => {
+    fetchData(
+      `search/photos?page=1&query=${searchValue}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+    );
+    setSearchValue("");
+    setSearchImage(searchValue);
+  };
+
+  const handleEnterChange = (e) => {
+    if (e.key === "Enter") {
+      fetchData(
+        `search/photos?page=1&query=${searchValue}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+      );
+      setSearchValue("");
+      setSearchImage(searchValue);
+    }
+  };
+
+  return (
+    <div className="flex">
+      <input
+        className="bg-gray-50 border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 rounded-tl  rounded-bl"
+        type="search"
+        placeholder="Search anything"
+        value={searchValue}
+        onChange={handleInputChange}
+        onKeyDown={handleEnterChange}
+      />
+      <button
+        onClick={handleButtonSearch}
+        disabled={!searchValue}
+        className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400"
+      >
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchField;
